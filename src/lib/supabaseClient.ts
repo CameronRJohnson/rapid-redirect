78a63462d9afbbe35eb5349577fd94dfda1d6540
Cameron Johnson <cajoro935@gmail.com>
2025-02-26 21:02:41 -0700

    Added passkeys because git removes them

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://nndfioyukugegxicmrqs.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5uZGZpb3l1a3VnZWd4aWNtcnFzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA1OTY3MTIsImV4cCI6MjA1NjE3MjcxMn0.VmpwiwN1M0GWNNzVIMWm5H98CE9FTja2bAoFqgI5zsY";
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error("Supabase URL and Anon Key are required.");
